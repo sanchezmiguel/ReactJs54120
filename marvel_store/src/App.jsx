8c75f45ce0745cceef3c8a5332e91ecc551000a7
Miguel Sanchez <msanchez@impulsedynamics.com>
2024-04-21 18:37:29 -0300

    se realiza correcion indicada por tutor

--- marvel_store/src/App.jsx
+++ marvel_store/src/App.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
-        <div className="app-container"> {}
+        <div className="app-container">
             <Navbar/>
             <ItemListContainer greeting='Bienvenido/a a nuestra tienda de Marvel'/>
         </div>
