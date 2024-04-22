686d1ea85d369fd1ceeb2f74eb50ab6f77920271
Miguel Sanchez <msanchez@impulsedynamics.com>
2024-04-22 08:30:11 -0300

    Actividad 4: sincronizar counter

--- marvel_store/src/components/itemCount/ItemCount.jsx
+++ marvel_store/src/components/itemCount/ItemCount.jsx
@@ -22,15 +22,15 @@ function ItemCount({ stock, initial, onAdd }) {
         <div className="item-count d-flex flex-column align-items-center justify-content-center p-3">
             <div className="d-flex align-items-center">
                 <button className="btn btn-secondary" onClick={handleDecrement} disabled={count <= 0}>
-                    <FontAwesomeIcon icon={faMinus} />
+                    <FontAwesomeIcon icon={faMinus}/>
                 </button>
                 <span className="mx-3">{count}</span>
                 <button className="btn btn-secondary" onClick={handleIncrement} disabled={count >= stock}>
-                    <FontAwesomeIcon icon={faPlus} />
+                    <FontAwesomeIcon icon={faPlus}/>
                 </button>
             </div>
             <button className="btn btn-primary mt-3" onClick={() => onAdd(count)} disabled={count === 0 || stock === 0}>
-                <FontAwesomeIcon icon={faShoppingCart} /> Agregar en Carrito
+                <FontAwesomeIcon icon={faShoppingCart}/> Agregar en Carrito
             </button>
         </div>
     );
--- marvel_store/src/components/itemDetail/ItemDetail.jsx
+++ marvel_store/src/components/itemDetail/ItemDetail.jsx
@@ -1,9 +1,14 @@
 // ItemDetail.jsx
-import React from 'react';
+import {useState} from 'react';
 import ItemCount from "../itemCount/ItemCount.jsx";
 import './ItemDetail.css';
 
 function ItemDetail({ item, onAdd }) {
+    const [itemAdded, setItemAdded] = useState(false);
+    const handleAdd = (quantity) => {
+        onAdd(quantity, item);
+        setItemAdded(true); // Indica que el ítem ha sido añadido
+    };
     return (
         <div className="item-detail card">
             <img src={item.imageUrl} alt={item.name} className="card-img-top" />
@@ -12,7 +17,14 @@ function ItemDetail({ item, onAdd }) {
                 <p className="card-text">{item.description}</p>
                 <p className="price text-success">${item.price}</p>
 
-                <ItemCount stock={item.stock} initial={item.initial} onAdd={(quantity) => onAdd(quantity, item)} />
+                {!itemAdded ? (
+                    <ItemCount stock={item.stock} initial={item.initial} onAdd={handleAdd} />
+                ) : (
+                    <div className="alert alert-success" role="alert">
+                        Producto añadido al carrito
+                    </div>
+                )}
+                {itemAdded && <button className="btn btn-success">Terminar mi compra</button>}
             </div>
         </div>
     );
