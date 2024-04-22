259b72318d441b60bca941b93359b90314d30966
Miguel Sanchez <msanchez@impulsedynamics.com>
2024-04-22 19:28:25 -0300

    fix
    ESLint: 'message' is missing in props validation(react/prop-types

--- marvel_store/src/components/alert/Alert.jsx
+++ marvel_store/src/components/alert/Alert.jsx
@@ -1,13 +1,17 @@
-// Alert.jsx Componente de Alertas Generico
-import React from 'react';
+import PropTypes from 'prop-types';
 import './Alert.css';
 
-const Alert = ({message, type}) => {
+const Alert = ({ message, type }) => {
     return (
         <div className={`alert ${type}`} role="alert">
             {message}
         </div>
     );
+};
+
+Alert.propTypes = {
+    message: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired
 };
 
 export default Alert;
