64897004e0aac704d3d4833ef0cd71994953dbea
Miguel Sanchez <msanchez@impulsedynamics.com>
2024-04-24 12:14:05 -0300

    agrego componentes para mejorar el codigo

---
+++ marvel_store/src/components/priceDisplay/PriceDisplay.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+const PriceDisplay = ({ price, currency = '$' }) => {
+    const formatPrice = (amount) => {
+        return `${currency}${amount.toFixed(2)}`;  // Formats to two decimal places
+    };
+
+    return (
+        <p className="price">{formatPrice(price)}</p>
+    );
+};
+
+PriceDisplay.propTypes = {
+    price: PropTypes.number.isRequired,
+    currency: PropTypes.string,
+};
+
+export default PriceDisplay;
---
+++ marvel_store/src/components/stockMessage/StockMessage.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+const StockMessage = ({ stock }) => {
+    const getMessage = () => {
+        if (stock > 1) {
+            return `${stock} unidades disponibles`;
+        } else if (stock === 1) {
+            return "Última unidad disponible";
+        } else {
+            return "No hay stock disponible";
+        }
+    };
+
+    return (
+        <p>{getMessage()}</p>
+    );
+};
+
+StockMessage.propTypes = {
+    stock: PropTypes.number.isRequired,
+};
+
+export default StockMessage;
