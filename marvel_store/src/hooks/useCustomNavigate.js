537f83122e6f69c7f93602d60c0d8be40df2f1e1
Miguel Sanchez <msanchez@impulsedynamics.com>
2024-04-24 12:13:46 -0300

    Agrego un hook de navegacion, que utiliza react-router-dom

---
+++ marvel_store/src/hooks/useCustomNavigate.js
@@ -0,0 +1,22 @@
+// src/hooks/useCustomNavigate.js
+import { useNavigate } from 'react-router-dom';
+
+const useCustomNavigate = () => {
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        // Potentially add logging here
+        console.log('Navigating back');
+        navigate(-1);
+    };
+
+    const goToHomePage = () => {
+        // Potentially add logging here
+        console.log('Navigating to home');
+        navigate('/');
+    };
+
+    return { goBack, goToHomePage };
+};
+
+export default useCustomNavigate;
