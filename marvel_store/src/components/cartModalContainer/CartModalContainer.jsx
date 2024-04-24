4e047ad949e849de2d935368ae0fb65564380aaa
Miguel Sanchez <msanchez@impulsedynamics.com>
2024-04-24 16:31:12 -0300

    Mejorar la presentacion del carrito

--- marvel_store/src/components/cartModalContainer/CartModalContainer.jsx
+++ marvel_store/src/components/cartModalContainer/CartModalContainer.jsx
@@ -2,7 +2,7 @@
 import CartModal from "../cartModal/CartModal.jsx";
 import {useCart} from "../../hooks/useCart.js";
 import PropTypes from "prop-types";
-
+import './CartModalContainer.css';
 
 const CartModalContainer = ({ isOpen, onClose }) => {
     const { cartItems } = useCart();
@@ -13,13 +13,15 @@ const CartModalContainer = ({ isOpen, onClose }) => {
     return (
         <CartModal isOpen={isOpen} onClose={onClose}>
             <div className="cart-content">
-                <h2>Your Cart</h2>
+                <h2>Contenido de tu Carrito</h2>
                 {cartItems.length > 0 ? (
                     <ul className="cart-items-list">
                         {cartItems.map(item => (
                             <li key={item.id} className="cart-item">
-                                <span className="item-name">{item.name}</span>
-                                <span className="item-quantity">{item.quantity} x ${item.price.toFixed(2)}</span>
+                                <div className="item-details">
+                                    <span className="item-name">{item.name}</span>
+                                    <span className="item-quantity">{item.quantity} x ${item.price.toFixed(2)}</span>
+                                </div>
                                 <span className="item-total-price">${(item.price * item.quantity).toFixed(2)}</span>
                             </li>
                         ))}
