4049bfecc37687fbcf0cc46c9fa0e7c30621fe9b
Miguel Sanchez <msanchez@impulsedynamics.com>
2024-05-22 19:04:23 -0300

    se mejora como se despliega el carrito y los elementos dentro de el

--- marvel_store/src/components/cartItem/CartItem.jsx
+++ marvel_store/src/components/cartItem/CartItem.jsx
@@ -2,13 +2,13 @@ import PropTypes from "prop-types";
 import PriceDisplay from "../priceDisplay/PriceDisplay.jsx";
 import './CartItem.css';
 
-const CartItem = ({ item, removeFromCart }) => (
+const CartItem = ({item, removeFromCart}) => (
     <li className="cart-item">
-        <img src={item.imageUrl} alt={item.name} className="cart-item-thumbnail" />
+        <img src={item.imageUrl} alt={item.name} className="cart-item-thumbnail"/>
         <div className="item-details">
             <span className="item-name">{item.name}</span>
-            <span className="item-quantity">{item.quantity} x <PriceDisplay price={item.price} /></span>
-            <span className="item-total-price"><PriceDisplay price={(item.price * item.quantity)} /></span>
+            <span className="item-quantity">{item.quantity} x <PriceDisplay price={item.price}/></span>
+            <span className="item-total-price"><PriceDisplay price={item.price * item.quantity}/></span>
         </div>
         <button onClick={() => removeFromCart(item.id)} className="remove-item">Quitar</button>
     </li>
